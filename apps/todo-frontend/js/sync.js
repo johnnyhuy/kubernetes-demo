330144d9716e051c94de080f5aa3f10e083e32f9
Johnny Huynh <27847622+johnnyhuy@users.noreply.github.com>
2022-03-08 08:11:22 +1100

    Remove this example

--- apps/todo-frontend/js/sync.js
+++
@@ -1,8 +0,0 @@
-function syncEntries() {
-  console.log("Synchronizing todo entries");
-  axios.get(API + "/api/todos").then(res => {
-    app.todos = res.data;
-  });
-};
-
-syncEntries();
---
+++ bin/helm-repo-add.sh
@@ -0,0 +1,2 @@
+
+helm repo add external-dns https://kubernetes-sigs.github.io/external-dns
