e4614e6e1bbc0669877a623c092662577c163768
Johnny Huynh <27847622+johnnyhuy@users.noreply.github.com>
2022-03-12 10:14:54 +1100

    Get Skaffold dev working

--- apps/todo-backend/src/main.ts
+++ apps/todo-backend/src/main.ts
@@ -4,6 +4,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(env.APP_PORT || 3000);
+  await app.listen(env.APP_PORT, env.APP_HOST);
 }
 bootstrap();
