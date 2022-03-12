e8d8c9642d853627e12d640f36173e3faf8aa1fd
Johnny Huynh <27847622+johnnyhuy@users.noreply.github.com>
2022-03-13 09:07:56 +1100

    Setup Mongo

--- apps/todo-backend/src/app.module.ts
+++ apps/todo-backend/src/app.module.ts
@@ -8,9 +8,7 @@ import { ConfigModule } from '@nestjs/config';
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    // MongooseModule.forRoot(
-    //   `mongodb://${env.MONGODB_HOST}/${env.MONGODB_DATABASE}`,
-    // ),
+    MongooseModule.forRoot(`mongodb://${env.MONGODB_HOST}`),
   ],
   controllers: [AppController],
   providers: [AppService],
