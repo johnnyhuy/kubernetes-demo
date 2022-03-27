eef4f008e0f53f116de5483d708a6c88b6bfe9f2
Johnny Huynh <27847622+johnnyhuy@users.noreply.github.com>
2022-03-27 05:04:04 +0000

    WIP add Jest in-memory testing

--- apps/todo-backend/src/todo/todo.controller.spec.ts
+++ apps/todo-backend/src/todo/todo.controller.spec.ts
@@ -1,22 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing'
-import { getModelToken, MongooseModule } from '@nestjs/mongoose'
+import { MongooseModule } from '@nestjs/mongoose'
 import { MongoMemoryServer } from 'mongodb-memory-server'
-import { Todo, TodoDocument } from './todo.schema'
 import { TodoModule } from './todo.module'
-import request from 'supertest'
-import { Model } from 'mongoose'
+import { FetchFunction, makeFetch } from 'supertest-fetch'
+import { INestApplication } from '@nestjs/common'
 
 describe('Todos controller', () => {
-  let todoModel: Model<TodoDocument>
-  let app: any
+  let app: INestApplication
+  let fetch: FetchFunction
 
   beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [
         MongooseModule.forRootAsync({
           useFactory: async () => {
-            const mongod = new MongoMemoryServer()
-            const uri = await mongod.getUri()
+            const mongod = await MongoMemoryServer.create()
+            const uri = mongod.getUri()
             return {
               uri: uri,
             }
@@ -27,26 +26,19 @@ describe('Todos controller', () => {
     }).compile()
 
     app = moduleFixture.createNestApplication()
-    todoModel = moduleFixture.get<Model<TodoDocument>>(getModelToken(Todo.name))
     await app.init()
+    fetch = makeFetch(app.getHttpServer())
   })
 
-  beforeEach(() => {
-    // return catModel.create(mockTodo);
-  })
-
-  afterEach(() => todoModel.remove({}))
-
-  it('GET /cats', () => {
-    return request(app.getHttpServer())
-      .get('/cats')
-      .expect(200)
-      .expect((res) => {
-        expect(res.body.length > 0).toBe(true)
-      })
-  })
-
-  afterAll(() => {
+  afterAll(async () => {
     app.close()
+  })
+
+  it('should get todos', async () => {
+    // Act
+    const response = await fetch('/todos')
+
+    // Assert
+    expect(response.statusText).toEqual('OK')
   })
 })
--- apps/todo-backend/src/todo/todo.service.spec.ts
+++ apps/todo-backend/src/todo/todo.service.spec.ts
@@ -1,4 +1,8 @@
+import { MongooseModule } from '@nestjs/mongoose'
 import { Test, TestingModule } from '@nestjs/testing'
+import { MongoMemoryServer } from 'mongodb-memory-server'
+import { TodoModule } from './todo.module'
+import { Todo, TodoSchema } from './todo.schema'
 import { TodoService } from './todo.service'
 
 describe('TodoService', () => {
@@ -6,7 +10,18 @@ describe('TodoService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TodoService],
+      imports: [
+        MongooseModule.forRootAsync({
+          useFactory: async () => {
+            const mongod = await MongoMemoryServer.create()
+            const uri = mongod.getUri()
+            return {
+              uri: uri,
+            }
+          },
+        }),
+        TodoModule,
+      ],
     }).compile()
 
     service = module.get<TodoService>(TodoService)
