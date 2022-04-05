34684dacde63b68ed39310d5fd26dc78b15ad0b5
Johnny Huynh <27847622+johnnyhuy@users.noreply.github.com>
2022-04-06 08:14:13 +1000

    Added chaos testing chart

--- bin/helm-repo-add.sh
+++ bin/helm-repo-add.sh
@@ -2,3 +2,4 @@
 
 helm repo add external-dns https://kubernetes-sigs.github.io/external-dns
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo add kubeinvaders https://lucky-sideburn.github.io/helm-charts
